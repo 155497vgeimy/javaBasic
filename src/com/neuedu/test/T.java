6d7817b1f6a35da25d444d21ca5cbdd25d3b3500
155497vgeimy <1098866154@qq.com>
2019-05-23 19:58:18 +0800

    zuoye

--- src/com/neuedu/test/T.java
+++ src/com/neuedu/test/T.java
@@ -2,6 +2,12 @@ package com.neuedu.test;
 
 public class T {
     public static void main(String[] args) {
-        System.out.println(2);
+        int a=123456;
+        int b=0;
+        while (a !=0){
+            b=a%10;
+            System.out.println(b);
+            a=a/10;
+        }
     }
 }
--- src/com/neuedu/test/Test.java
+++ src/com/neuedu/test/Test.java
@@ -2,6 +2,13 @@ package com.neuedu.test;
 
 public class Test {
     public static void main(String[] args) {
-        System.out.println(1);
+        int a=153;
+        int b=0;
+        while (a !=0) {
+            b=a%10;
+            System.out.print(b);
+            a=a/10;
+        }
+
     }
 }
