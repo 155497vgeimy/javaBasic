cd668b33718ce8d4f8027d1df51a6d763c1ed01d
155497vgeimy <1098866154@qq.com>
2019-05-29 08:06:49 +0800

    zuoye

--- src/com/neuedu/test1/Bijiben.java
+++ src/com/neuedu/test1/Bijiben.java
@@ -12,18 +12,6 @@ public class Bijiben {
     public Bijiben(char    color, int   cpu){
         this.color=color;
         this.cpu=cpu;
-
-//    public void setColor(){
-//        this.color=color;
-//    }
-//    public char getColor(){
-//        return color;
-//    }
-//    public void setCpu(){
-//        this.cpu=cpu;
-//    }
-//    public int getCpu(){
-//        return cpu;
     }
     public String toString() {
         return "颜色为："+color+"  "+"cpu型号为："+cpu;
