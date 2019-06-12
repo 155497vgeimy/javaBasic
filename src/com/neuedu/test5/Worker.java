36c5799fd6d0558b11c30c625cb7d10f07b62ecd
155497vgeimy <1098866154@qq.com>
2019-06-12 08:14:49 +0800

    zuoye

---
+++ src/com/neuedu/test5/Worker.java
@@ -0,0 +1,32 @@
+package com.neuedu.test5;
+
+public class Worker {
+    private String name;
+    private int age;
+    private int price;
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
+
+
+}
---
+++ src/com/neuedu/test5/Workerclass.java
@@ -0,0 +1,9 @@
+package com.neuedu.test5;
+
+import java.util.LinkedList;
+
+public class Workerclass {
+    public static void main(String[] args) {
+        
+    }
+}
